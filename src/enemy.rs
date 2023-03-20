3d0d65809982ae55aae6a6855cb76a812bf5144a
ShadowCurse <shadowcurse57@gmail.com>
2023-03-20 23:53:41 +0000

    moving enemies

--- src/enemy.rs
+++ src/enemy.rs
@@ -4,18 +4,21 @@ use bevy_rapier2d::prelude::*;
 use crate::player::Player;
 
 pub const ENEMY_HEALTH: i64 = 100;
+pub const ENEMY_SPEED: f32 = 69.0;
+pub const ENEMY_MOVEMENT_FORCE: f32 = 1000.0;
 
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_enemies);
+        app.add_systems((enemy_spawn, enemy_movement));
     }
 }
 
 #[derive(Component)]
 pub struct Enemy {
     pub health: i64,
+    pub speed: f32,
 }
 
 #[derive(Component)]
@@ -25,7 +28,7 @@ pub struct EnemyWave {
     pub timer: Timer,
 }
 
-fn spawn_enemies(
+fn enemy_spawn(
     time: Res<Time>,
     mut commands: Commands,
     mut wave: Query<(&Transform, &mut EnemyWave), With<Player>>,
@@ -44,6 +47,7 @@ fn spawn_enemies(
         commands
             .spawn(RigidBody::Dynamic)
             .insert(Collider::ball(10.0))
+            .insert(Velocity::default())
             .insert(Damping {
                 linear_damping: 10.0,
                 angular_damping: 1.0,
@@ -51,6 +55,23 @@ fn spawn_enemies(
             .insert(TransformBundle::from(Transform::from_translation(position)))
             .insert(Enemy {
                 health: ENEMY_HEALTH,
+                speed: ENEMY_SPEED,
             });
+    }
+}
+
+fn enemy_movement(
+    time: Res<Time>,
+    player: Query<&Transform, With<Player>>,
+    mut enemies: Query<(&Enemy, &Transform, &mut Velocity)>,
+) {
+    let player_transform = player.single();
+
+    for (enemy, enemy_transform, mut enemy_velocity) in enemies.iter_mut() {
+        let direction = (player_transform.translation - enemy_transform.translation)
+            .truncate()
+            .normalize();
+        let movement = direction * time.delta().as_secs_f32();
+        enemy_velocity.linvel = movement * enemy.speed * ENEMY_MOVEMENT_FORCE;
     }
 }
